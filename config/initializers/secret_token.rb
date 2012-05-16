6a4699e67132e0c7aa9da85bc7e1e7c518a2f9db
Patrik Björklund <p.bjorklund@gmail.com>
2012-05-16 16:24:12 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kodhjalpen::Application.config.secret_token = 'ad9b6e0cb728f9dc6d8ebda471b52ae8aa801c3023dd5087b720ef99e88d9cc1c379d0ee96a67cd2f51cc92435cec6fc3bd7d10aed0dc8abd0925332134bce8b'
