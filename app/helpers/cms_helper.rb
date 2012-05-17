97750cb36efb5b295604108db9491b4b2305a0fd
Patrik Björklund <p.bjorklund@gmail.com>
2012-05-17 18:10:03 +0200

    Build navigation from cms pages

--- app/helpers/cms_helper.rb
+++ app/helpers/cms_helper.rb
@@ -1,8 +1,9 @@
 module CmsHelper
-  def get_page_links
-    @links = @cms_site.pages.root.children.published.map do |page|
-        link_to page.label, page.full_path 
+  def get_navigation
+    @cms_site.pages.root.children.published.map do |page|
+      css_class = current_page?(page.full_path) ? "active" : ""
+      link = link_to page.label, page.full_path 
+      content_tag 'li', link, class: css_class
     end
-    @links
   end
 end
