e5a4f00ddb0ba083490657ba82338aa475e8055e
Patrik Björklund <p.bjorklund@gmail.com>
2012-05-26 12:12:30 +0200

    Update user.feature to pass

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,13 +1,22 @@
 # encoding: utf-8
+Given /^I am on the "(.*?)" page$/ do |page|
+  [Cms::Site, Cms::Page, Cms::Layout].map(&:delete_all)
+  FactoryGirl.create(:site_with_page)
+  visit page
+end
 
 When /^I click on "(.*?)"$/ do |link|
   click_link link
 end
 
 When /^I fill in my details$/ do
-  fill_in "Email",                  with: "p.bjorklund@gmail.com"
-  fill_in "Password",               with: "password"
-  fill_in "Password confirmation",  with: "password"
+  fill_in "Förnamn",           with: "Patrik"
+  fill_in "Efternamn",         with: "Björklund"
+  fill_in "Sysselsättning",    with: "Konsult"
+  fill_in "Stad",              with: "Göteborg"
+  fill_in "Epost",             with: "p.bjorklund@gmail.com"
+  fill_in "Lösenord",          with: "password"
+  fill_in "Upprepa lösenord",  with: "password"
 
   click_on "Börja göra nytta"
 end
--- features/user.feature
+++ features/user.feature
@@ -2,8 +2,7 @@ Feature: User signup
   As a user, I want to register as a user, to volunteer my coding skills for charity
 
   Scenario: Signing up
-    Given I am on the startpage
-    When  I click on "User signup"
-    And   I fill in my details
+    Given I am on the "/developers/new" page
+    When  I fill in my details
     Then  I should see a alert-success message
     And   I should see my email in the navigation
