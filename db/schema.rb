80b7dbc590711a2d9868135d933a2406096547db
Patrik Björklund <p.bjorklund@gmail.com>
2012-06-03 13:43:29 +0200

    Schema update after migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,29 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120531151530) do
+ActiveRecord::Schema.define(:version => 20120603112941) do
 
   create_table "charities", :force => true do |t|
     t.string   "name"
     t.string   "mission"
     t.string   "needs_help_with"
     t.string   "city"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
-
-  add_index "charities", ["email"], :name => "index_charities_on_email", :unique => true
-  add_index "charities", ["reset_password_token"], :name => "index_charities_on_reset_password_token", :unique => true
 
   create_table "cms_blocks", :force => true do |t|
     t.integer  "page_id",    :null => false
